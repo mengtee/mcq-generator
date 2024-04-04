8a954645b03096c8cc0e0891ac1481e669b8802e
mengtee <mengtee1127@gmail.com>
2024-04-04 16:58:54 +0800

    updated file

--- setup.py
+++ setup.py
@@ -1,1 +1,12 @@
 # Installing local package in a local environment
+
+from setuptools import find_packages, setup
+
+setup(
+    name="mcqgenerator",
+    version="0.0.1",
+    author="meng kiat",
+    author_email="mengtee1127@gmail.com",
+    install_requires=["openai", "langchain","streamlit", "python-dotenv",'PyPDF2'],
+    package=find_packages()
+)
